1e8da88bf2c6b0d36d483b813ae1bac55a7c1849
Abduqodir65 <abduqodirgithub@gmail.com>
2025-01-04 08:50:31 +0500

    config sozlandi va databasage ulandi

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,38 @@
+import { appConfig, databaseConfig } from '@config';
 import { Module } from '@nestjs/common';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, databaseConfig],
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        try {
+          return {
+            dialect: 'postgres',
+            host: config.get<string>('databaseConfig.host'),
+            port: config.get<number>('databaseConfig.port'),
+            username: config.get<string>('databaseConfig.user'),
+            password: config.get<string>('databaseConfig.password'),
+            database: config.get<string>('databaseConfig.dbname'),
+            models: [],
+            synchronize: true,
+            logging: console.log,
+            autoLoadModels: true,
+          };
+        } catch (error) {
+          console.error('Error occurred while connecting to the database', error);
+          throw error;
+        }
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
